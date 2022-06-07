12338abe0cf020a4ff2ffd966067a59b6868a958
StanleyKhashane <Stanleyoda70@gmail.com>
2022-06-07 06:04:43 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,16 @@
-#include <stdio.h>                                                                                                                      
-                                                                                                                                                                                                                                                                                
-/**                                                                                                                                     
- * main -Entry point of the program                                                                                                     
- *                                                                                                                                      
- * Description: This program prints _putchar and ends with a new line                                                                   
- *                                                                                                                                      
- * Return: Aways returns(0)                                                                                                             
- */                                                                                                                                     
-                                                                                                                                        
-int main(void)                                                                                                                          
-{                                                                                                                                       
-        printf("_putchar('\n')");                                                                                                       
-        return (0);
+#include <stdio.h>
+
+/**
+ * main -Entry point of the program
+ *
+ * Description: This program prints _putchar and ends with a new line
+ *
+ * Return: Aways returns(0)
+ */
+
+int main(void)
+{
+	printf("_putchar('\n')");
+	return (0);
 }
+
