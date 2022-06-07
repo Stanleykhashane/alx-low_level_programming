5467fae910206281e26c7f8f831791212d058c22
Stanleykhashane <103378810+Stanleykhashane@users.noreply.github.com>
2022-06-07 14:49:24 +0200

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>                                                                                                                      
+                                                                                                                                                                                                                                                                                
+/**                                                                                                                                     
+ * main -Entry point of the program                                                                                                     
+ *                                                                                                                                      
+ * Description: This program prints _putchar and ends with a new line                                                                   
+ *                                                                                                                                      
+ * Return: Aways returns(0)                                                                                                             
+ */                                                                                                                                     
+                                                                                                                                        
+int main(void)                                                                                                                          
+{                                                                                                                                       
+        printf("_putchar('\n')");                                                                                                       
+        return (0);
+}
